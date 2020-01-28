32720e3ceeb4c1a9596f2f7db928ba506d466dee
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-01-28 06:28:30 -0800

    Using stateless functional Components in React JS

--- src/Cities.js
+++ src/Cities.js
@@ -1,24 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Cities extends Component {
-    render() {
-        const { cities } = this.props; // De-Structurelisation
-        const htmlCitiesList = cities.map(city => {
-            return (
-                <div className="cities">
-                    <div>Name :  {city.name}</div>
-                    <div>Destination : {city.destination}</div>
-                    <div>Country : {city.country}</div>
-                </div>
-            );
-        });
-
+const Cities = (props) => { 
+    /*
+    If we want to use destruturing the props at the paramter of the functional 
+    component then we can do like
+const Cities = ({cities}) => {
+    ........
+}
+    */
+    const { cities } = props; // De-Structurelisation
+    const htmlCitiesList = cities.map(city => {
         return (
-            <div className="City-List">
-                {htmlCitiesList}
+            <div className="cities">
+                <hr></hr>
+                <div>Name :  {city.name}</div>
+                <div>Destination : {city.destination}</div>
+                <div>Country : {city.country}</div>
+                <hr></hr>
             </div>
         );
-    }
+    });
+    return (
+        <div className="City-List">
+            {htmlCitiesList}
+        </div>
+    );
 }
 
+// class Cities extends Component {
+//     render() {
+//         const { cities } = this.props; // De-Structurelisation
+//         const htmlCitiesList = cities.map(city => {
+//             return (
+//                 <div className="cities">
+//                 <hr></hr>
+//                     <div>Name :  {city.name}</div>
+//                     <div>Destination : {city.destination}</div>
+//                     <div>Country : {city.country}</div>
+//                 <hr></hr>
+//                 </div>
+//             );
+//         });
+
+//         return (
+//             <div className="City-List">
+//                 {htmlCitiesList}
+//             </div>
+//         );
+//     }
+// }
 export default Cities;
